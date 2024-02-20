d73f54aef8170104291228e32f1a97bc2c4bbc63
Tarashrita Ganguly <150145692+Tarashrita-Ganguly@users.noreply.github.com>
2024-02-20 17:50:07 +0530

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -53,8 +53,16 @@ var gameConfig={
 =            Game Status             =
 ====================================*/
 
-function game(){
+NoseX="";
+NoseY="";
+gameStatus="";
 
+function startGame() {
+  gameStatus="start";
+  document.getElementById("status").innerHTML="Game is loading";
+}
+
+function game(){
   instializeInDraw();
   moveEnvironment(mario);
   drawSprites();
@@ -67,7 +75,7 @@ function game(){
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press Play button to start the game ", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -115,7 +123,7 @@ function game(){
 
 // change game status if any key is pressed
 function changeGameStatud(character){
-  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
+  if(gameStatus == "start"&& NoseX!="" && gameConfig.status==="start") {
     world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
@@ -279,19 +287,19 @@ function autoControl(character){
 function manualControl(character){
   
   if(character.live){
-    if(keyDown(control.left)){
+    if(NoseX<300){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(keyDown(control.right)){
+    if(NoseX>300){
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
     }
 
-    if(!keyDown(control.left)&&!keyDown(control.right)&&!keyDown(control.up)){ 
+    if(NoseX<300 && NoseX>300 && NoseY<150){ 
       character.changeAnimation('stand');
     }
   }
@@ -300,7 +308,7 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
+	if( (NoseY<150 && character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
 	}
 }
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
@@ -8,6 +9,25 @@ function setup() {
 	canvas = createCanvas(1240,336);
 	instializeInSetup(mario);
 	canvas.parent('canvas');
+
+	video=createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet('video',modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log('model loaded!');
+}
+
+function gotPoses(results) {
+	if(results.length > 0){
+		console.log(results);
+		NoseX = results[0].pose.nose.x;
+		NoseY = results[0].pose.nose.y;
+	}
 }
 
 function draw() {
